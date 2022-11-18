d886d0e41b7c2454fd135e3f9f3838ffe5b08877
Nikolai Kamilatov <nicko1608@yandex.ru>
2022-11-19 02:07:35 +0400

    finifshed

--- src/main/java/ru/nickkaml/Main.java
+++ src/main/java/ru/nickkaml/Main.java
@@ -7,7 +7,8 @@ public class Main {
         int intNumber = -2147483648;
         long longNumber = -9223372036854775808L;
         float floatNumber = Float.MAX_VALUE;
-        double doubleNumber = 100.005d;
+        double doubleNumber = Double.MAX_VALUE;
+
 
         //переполнение byte
         byteNumber = (byte) (byteNumber - 1);
@@ -25,11 +26,22 @@ public class Main {
         longNumber = longNumber - 1L;
         System.out.println("Вернулись к верхней границе диапазона для long = " + longNumber); // = 9223372036854775807
 
+        //переполнение floatNumber
+        floatNumber = floatNumber + floatNumber;
+        System.out.println("Переполнение для float = " + floatNumber); // = Infinity
+
+        //переполнение doubleNumber
+        doubleNumber = doubleNumber + doubleNumber;
+        System.out.println("Переполнение для double = " + doubleNumber); // = Infinity
 
 
+        // умножение int и double
+        int a = 2;
+        double b = 400.5;
+        System.out.println("2 x 400.5 = " + (a * b)); // = 801.0
 
-
-
+        //сравнение int и double
+        System.out.println("2 <= 400.5 = " + (a <= b)); // = true
 
 
     }
